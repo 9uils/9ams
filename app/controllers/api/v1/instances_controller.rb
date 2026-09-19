5d2076573b8107f60739d0056ad9d4692b0ca623
9uils <mndrnorng@gmail.com>
2026-09-19 19:04:30 +0900

    Update instances_controller.rb

--- app/controllers/api/v1/instances_controller.rb
+++ app/controllers/api/v1/instances_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-class Api::V1::InstancesController < Api::V2::InstancesController
-  skip_before_action :require_authenticated_user!, only: [:show]
-  
+class Api::V1::InstancesController < Api::V2::InstancesController  
   include DeprecationConcern
 
   deprecate_api '2022-11-14'
