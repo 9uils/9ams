a2ae08c1dc9aae5fba9778c812cbf4d328308da8
9uils <mndrnorng@gmail.com>
2026-09-19 16:40:58 +0900

    Add files via upload

--- app/javascript/mastodon/components/account.tsx
+++ app/javascript/mastodon/components/account.tsx
@@ -262,7 +262,7 @@ export const Account: React.FC<{
               </div>
             )}
 
-            {account?.note && (
+            {account?.note && !inReplyTo && (
               <div className="account__bio" dangerouslySetInnerHTML={{ __html: account.note }} />
             )}
           </div>
